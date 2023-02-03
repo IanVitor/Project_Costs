eb495459a7c11f4fafe20b25f364c1186a2cb0f6
Ian Vitor Moraes da Silva <76691828+IanVitor@users.noreply.github.com>
2023-02-03 03:52:56 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
+import NavBar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+
+        <NavBar />
+
+        <Container customClass="min_height">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
+            <Route path="/company" element={<Company />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/newProject" element={<NewProject />}></Route>
+            <Route path="/project/:id" element={<Project />}></Route>
+          </Routes>
+        </Container>
+        <Footer />
+      </Router>
+    </div>
+  )
+}
+
+export default App;
